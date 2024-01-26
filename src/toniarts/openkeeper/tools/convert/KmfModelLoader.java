9ce878141b4a945615148204353c3b7f55ac3b30
Trass3r <trass3r@gmail.com>
2024-01-26 06:19:28 +0100

    remove morph target for frame 0

--- src/toniarts/openkeeper/tools/convert/KmfModelLoader.java
+++ src/toniarts/openkeeper/tools/convert/KmfModelLoader.java
@@ -258,6 +258,7 @@ public final class KmfModelLoader implements AssetLoader {
         final int lastFrame = anim.getFrames() - 1;
         assert lastFrame > 0;
         final int numFrames = (lastFrame - 1) / frameSubdiv + 1 + 1; // ceiling division + 1
+        final int numMorphTracks = numFrames - 1; // first frame doesn't need one
         float[] times = new float[numFrames];
         for (int i = 0; i < numFrames-1; ++i)
             times[i] = (frameSubdiv * i) / 30f;
@@ -290,6 +291,13 @@ public final class KmfModelLoader implements AssetLoader {
 
                 ++i;
             }
+
+            // set up weights as identity matrix
+            // frames are columns, morph targets are rows
+            // first column is 0
+            var weights = new float[numFrames * numMorphTracks];
+            for (i = 0; i < numMorphTracks; ++i)
+                weights[i * numFrames + i+1] = 1;
 
             // now get the vertices for each frame, make sure we pick the last frame too
             for (int frame = 0; frame < anim.getFrames(); frame += Math.max(1, Math.min(frameSubdiv, anim.getFrames() - frame - 1)))
@@ -326,6 +334,7 @@ public final class KmfModelLoader implements AssetLoader {
                     mesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
                     for (i = 0; i < vertices.length; ++i)
                         baseVertices[i] = new Vector3f(vertices[i]);
+                    continue;
                 }
                 // create a relative morph target
                 var morphTarget = new MorphTarget("submesh " + subMeshIndex + " frame " + frame);
@@ -348,13 +357,7 @@ public final class KmfModelLoader implements AssetLoader {
             // Create geometry
             Geometry geom = createGeometry(subMeshIndex, anim.getName(), mesh, materials, subMesh.getMaterialIndex());
 
-            // Create a morph track for this mesh
-            var weights = new float[numFrames * numFrames];
-            // set up weights as identity matrix
-            for (i = 1; i < numFrames; ++i) {
-                weights[i * numFrames + i] = 1;
-            }
-            var morphTrack = new MorphTrack(geom, times, weights, numFrames);
+            var morphTrack = new MorphTrack(geom, times, weights, numMorphTracks);
             animTracks.add(morphTrack);
 
             //Attach the geometry to the node
