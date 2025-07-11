e67b35d468d6ac82a17de2e06fcd4fe5138908e2
Trass3r <trass3r@gmail.com>
2025-07-11 02:16:56 +0200

    try setAudioRenderer(null)

--- src/toniarts/openkeeper/Main.java
+++ src/toniarts/openkeeper/Main.java
@@ -239,6 +239,7 @@ public final class Main extends SimpleApplication {
 
         // Init the user settings (which in JME are app settings)
         app.settings = Settings.getInstance().getAppSettings();
+        app.settings.setAudioRenderer(null);
     }
 
     /**
